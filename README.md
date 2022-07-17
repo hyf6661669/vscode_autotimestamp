70dd4c3dbf4e391e1061fa26661a2094655a19be
HYF <hyf_sysu@qq.com>
2022-07-18 07:51:41 +0800

    上传插件

---
+++ README.md
@@ -0,0 +1,29 @@
+# Auto Time Stamp
+[![Build status](https://ci.appveyor.com/api/projects/status/8jhbugo5d2ejuylh?svg=true)](https://ci.appveyor.com/project/lpubsppop01/vscode-auto-timestamp)
+
+A Visual Studio Code extension that update time stamp in file content when saving document.
+
+## Features
+When saving document:
+  - Update last modified time field
+  - Fill birth time field by file time stamp if it is empty
+
+Each of the fields will be detected with condition of settings.
+By default settings, lines like the following will be detected:
+```
+// Created: 2018/02/04 12:24:41
+// Last modified: 2018/02/09 11:41:41
+```
+
+If it does not work, please check the Line Limit setting. This setting is 5 lines from the beginning of the file by default, so it may be too small for your file.
+
+In addition to the above, there is also the setting of the target file name (Filename Pattern), but by default all files are matched.
+
+## Download
+[vscode-auto-timestamp Latest Build - AppVeyor](https://ci.appveyor.com/api/projects/lpubsppop01/vscode-auto-timestamp/artifacts/vscode-auto-timestamp-0.0.4.vsix)
+
+## Author
+[lpubsppop01](https://github.com/lpubsppop01)
+
+## License
+[zlib License](https://github.com/lpubsppop01/vscode-auto-timestamp/raw/master/LICENSE.txt)
---
+++ out/extension.js
@@ -0,0 +1,133 @@
+'use strict';
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.deactivate = exports.activate = void 0;
+const vscode = require("vscode");
+const fs = require("fs");
+const moment = require("moment");
+function activate(context) {
+    const config = new ExtensionConfiguration();
+    context.subscriptions.push(vscode.workspace.onDidChangeConfiguration(e => {
+        config.onDidChangeConfiguration();
+    }));
+    const core = new ExtensionCore(config);
+    context.subscriptions.push(vscode.workspace.onWillSaveTextDocument(e => {
+        core.onWillSaveTextDocument(e);
+    }));
+}
+exports.activate = activate;
+function deactivate() {
+}
+exports.deactivate = deactivate;
+class ExtensionCore {
+    constructor(config) {
+        this.m_config = config;
+    }
+    onWillSaveTextDocument(e) {
+        if (!e.document.fileName.match(this.m_config.fileNamePattern))
+            return;
+        var edits = [];
+        const lineIndices = this.getIndexRangeUntil(this.m_config.lineLimit, e.document.lineCount);
+        for (const iLine of lineIndices) {
+            const line = e.document.lineAt(iLine);
+            const birthTimeRange = this.getTextRangeBetween(line, this.m_config.birthTimeStart, this.m_config.birthTimeEnd);
+            if (birthTimeRange != null && birthTimeRange.isEmpty) {
+                const stats = fs.statSync(e.document.fileName);
+                const timeStr = moment(stats.birthtime).format(this.m_config.momentFormat);
+                edits.push(vscode.TextEdit.replace(birthTimeRange, timeStr));
+            }
+            const modifiedTimeRange = this.getTextRangeBetween(line, this.m_config.modifiedTimeStart, this.m_config.modifiedTimeEnd);
+            if (modifiedTimeRange != null) {
+                const timeStr = moment().format(this.m_config.momentFormat);
+                edits.push(vscode.TextEdit.replace(modifiedTimeRange, timeStr));
+            }
+        }
+        e.waitUntil(Promise.resolve(edits));
+    }
+    getIndexRangeUntil(limit, count) {
+        const indices = [];
+        if (limit > 0) {
+            let i = 0;
+            const iMax = Math.min(limit, count);
+            while (i < iMax) {
+                indices.push(i++);
+            }
+        }
+        else {
+            let i = count - 1;
+            const iMin = Math.max(count + limit, 0);
+            while (i >= iMin) {
+                indices.push(i--);
+            }
+        }
+        return indices;
+    }
+    getTextRangeBetween(line, startPattern, endPattern) {
+        const startResult = line.text.match(startPattern);
+        if (startResult == null)
+            return null;
+        const iRangeStart = startResult.index + startResult[0].length;
+        const endResult = line.text.substr(iRangeStart).match(endPattern);
+        if (endResult == null)
+            return null;
+        const iRangeEnd = iRangeStart + endResult.index;
+        const startPos = new vscode.Position(line.lineNumber, iRangeStart);
+        var endPos = new vscode.Position(line.lineNumber, iRangeEnd);
+        return new vscode.Range(startPos, endPos);
+    }
+}
+class ExtensionConfiguration {
+    constructor() {
+        this.m_config = vscode.workspace.getConfiguration("lpubsppop01.autoTimeStamp");
+    }
+    onDidChangeConfiguration() {
+        this.m_config = vscode.workspace.getConfiguration("lpubsppop01.autoTimeStamp");
+        this.m_fileNamePattern = null;
+        this.m_birthTimeStart = null;
+        this.m_birthTimeEnd = null;
+        this.m_modifiedTimeStart = null;
+        this.m_modifiedTimeEnd = null;
+    }
+    getValue(propertyName, defaultValue) {
+        if (this.m_config == null)
+            return defaultValue;
+        const value = this.m_config.get(propertyName);
+        return value != null ? value : defaultValue;
+    }
+    get fileNamePattern() {
+        if (this.m_fileNamePattern == null) {
+            this.m_fileNamePattern = new RegExp(this.getValue("filenamePattern", ".*"));
+        }
+        return this.m_fileNamePattern;
+    }
+    get lineLimit() {
+        return this.getValue("lineLimit", 10);
+    }
+    get birthTimeStart() {
+        if (this.m_birthTimeStart == null) {
+            this.m_birthTimeStart = new RegExp(this.getValue("birthTimeStart", "[cC]reated[ -][tT]ime.*: "));
+        }
+        return this.m_birthTimeStart;
+    }
+    get birthTimeEnd() {
+        if (this.m_birthTimeEnd == null) {
+            this.m_birthTimeEnd = new RegExp(this.getValue("birthTimeEnd", "$"));
+        }
+        return this.m_birthTimeEnd;
+    }
+    get modifiedTimeStart() {
+        if (this.m_modifiedTimeStart == null) {
+            this.m_modifiedTimeStart = new RegExp(this.getValue("modifiedTimeStart", "[lL]ast[ -][mM]odified[ -][tT]ime.*: "));
+        }
+        return this.m_modifiedTimeStart;
+    }
+    get modifiedTimeEnd() {
+        if (this.m_modifiedTimeEnd == null) {
+            this.m_modifiedTimeEnd = new RegExp(this.getValue("modifiedTimeEnd", "$"));
+        }
+        return this.m_modifiedTimeEnd;
+    }
+    get momentFormat() {
+        return this.getValue("momentFormat", "YYYY-MM-DD HH:mm:ss");
+    }
+}
+//# sourceMappingURL=extension.js.map
